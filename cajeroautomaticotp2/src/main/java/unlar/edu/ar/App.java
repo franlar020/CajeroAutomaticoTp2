9110cfb5099714e1a68e95427b8bfb49f75694f1
Francisco Antonio Gonzalez <franlar020@gmail.com>
2026-04-23 23:41:27 -0300

    Eliminar mensaje de saludo y agregar inicialización de servicios y cuentas bancarias en la clase App

--- cajeroautomaticotp2/src/main/java/unlar/edu/ar/App.java
+++ cajeroautomaticotp2/src/main/java/unlar/edu/ar/App.java
@@ -1,13 +1,23 @@
 package unlar.edu.ar;
 
-/**
- * Hello world!
- *
- */
+import unlar.edu.ar.service.CajeroService;
+import unlar.edu.ar.ui.MenuPrincipal;
+import unlar.edu.ar.model.CuentaBancaria;
+import java.util.ArrayList;
+
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        CajeroService service = new CajeroService();
+        MenuPrincipal ui = new MenuPrincipal();
+
+        // Cuentas bancarias de prueba
+        CuentaBancaria cuenta1 = new CuentaBancaria("Ismael Flores", "1234567890", 5000.00, true, "1234", new ArrayList<>());
+        CuentaBancaria cuenta2 = new CuentaBancaria("Francisco Antonio Gonzalez", "0987654321", 3000.00, true, "5678", new ArrayList<>());
+        CuentaBancaria cuenta3 = new CuentaBancaria("Virginia Vera", "1122334455", 7000.00, true, "9012", new ArrayList<>());
+
+        ui.iniciar(cuenta3);
     }
 }
